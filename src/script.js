d028f782737baf8e07a4a773cef73d5166ed3cb1
Neeiser <stortini.study@gmail.com>
2022-08-02 15:34:20 +0200

    Animation with standard time + Clock function

--- src/script.js
+++ src/script.js
@@ -1,5 +1,6 @@
 import './style.css'
 import * as THREE from 'three'
+/* import gsap from 'gsap' */
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
@@ -29,4 +30,41 @@ const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
-renderer.render(scene, camera)
+
+
+
+// Animation
+
+let time = Date.now() //Per normalizzare il framerate con il metodo del tempo
+
+//Metodo Clock
+const clock = new THREE.Clock()
+
+const tick = () =>
+{
+    const currentTime = Date.now()
+    const deltaTime = currentTime - time
+    time = currentTime
+
+    mesh.rotation.y += 0.001 * deltaTime
+
+    //Metodo Clock
+
+     
+    const elapsedTime = clock.getElapsedTime()
+    
+    // mesh.rotation.y = elapsedTime
+    
+
+    // Effetto seno/coseno, va su e torna giù di continuo, a LOOP.
+    mesh.position.x = Math.sin(elapsedTime)
+    mesh.position.y = Math.cos(elapsedTime)
+    
+
+    //Renderizzare ad ogni tick
+    renderer.render(scene, camera)
+
+    window.requestAnimationFrame(tick)
+}
+
+tick()
